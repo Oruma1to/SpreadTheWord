602229aea98efebc847e649d65c03af83fcd5081
Oruma1to <60720664+Oruma1to@users.noreply.github.com>
2020-06-10 12:05:54 -0400

    comment migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_155440) do
+ActiveRecord::Schema.define(version: 2020_06_10_160507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
