138cb428e16323752975163afd41e05d5b942582
Oruma1to <60720664+Oruma1to@users.noreply.github.com>
2020-06-18 23:48:29 -0400

    css for edit complete

--- client/src/Article/PostEdit.jsx
+++ client/src/Article/PostEdit.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { getPost, updatePost } from '../services/posts'
+import './PostEdit.css'
 
 class PostEdit extends Component {
   constructor(props) {
@@ -40,57 +41,62 @@ class PostEdit extends Component {
       return <Redirect to={`/posts/${this.props.match.params.id}`} />
     }
     return (
-      <div className="product-edit">
-        <img className="edit-product-image" src={post.img_url} alt={post.title} />
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            let { id } = this.props.match.params
-            const updated = updatePost(id, this.state.post)
-            history.push(`/posts/${post.id}`)
-            this.setState({ updated })
-          }}>
-          <label htmlFor="img_url">
-            <input
-              className="edit-input-image-link"
-              placeholder='Image Url'
-              value={post.img_url}
-              name='img_url'
-              required
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <label htmlFor="title">
-            <input
-              className="input-name"
-              placeholder='Name'
-              value={post.title}
-              name='title'
-              required
-              autoFocus
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <label htmlFor="content">
-            <textarea
-              className="textarea-content"
-              rows={10}
-              cols={78}
-              placeholder='Content'
-              value={post.content}
-              name='content'
-              required
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <div>
-            <button type='submit' className="save-button">Save</button>
-          </div>
-        </form>
-      </div>
+      <>
+        <main className="postEdit-container">
+          <img className="postEdit-image" src={post.img_url} alt={post.title} />
+          <form className="postEdit-form"
+            onSubmit={(e) => {
+              e.preventDefault()
+              let { id } = this.props.match.params
+              const updated = updatePost(id, this.state.post)
+              history.push(`/posts/${post.id}`)
+              this.setState({ updated })
+            }}>
+            <p className="postEdit-formTitle">Image Url</p>
+            <label htmlFor="img_url">
+              <input
+                className="postEdit-imglink"
+                placeholder='Image Url'
+                value={post.img_url}
+                name='img_url'
+                required
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <p className="postEdit-formTitle">Post Title</p>
+            <label htmlFor="title">
+              <input
+                className="postEdit-title"
+                placeholder='Name'
+                value={post.title}
+                name='title'
+                required
+                autoFocus
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <p className="postEdit-formTitle">Content</p>
+            <label htmlFor="content">
+              <textarea
+                className="postEdit-content"
+                rows={10}
+                cols={78}
+                placeholder='Content'
+                value={post.content}
+                name='content'
+                required
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <div className="buttonDiv">
+              <button type='submit' className="postEdit-save">Save</button>
+            </div>
+          </form>
+        </main>
+      </>
     )
   }
 }
